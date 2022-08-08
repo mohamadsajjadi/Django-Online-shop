cc9b263c0a67489addc0a945eeeaa1240a57484c
Mohamad <alisajadian49@yahoo.com>
2022-08-08 18:21:39 +0430

    make a custom Manager for User

--- A/accounts/models.py
+++ A/accounts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from .managers import UserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=11, unique=True)
+    full_name = models.CharField()
+    is_active = models.BooleanField()
+    is_admin = models.BooleanField()
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['email', 'full_name']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
