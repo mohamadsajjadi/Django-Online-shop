4b6fdaac5f7f8238bd8b448f61efddf29fbb1c23
Mohamad <alisajadian49@yahoo.com>
2022-08-10 23:39:52 +0430

    make a detail page for each product

--- A/home/views.py
+++ A/home/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+from .models import Product
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home/home.html')
+        product = Product.objects.filter(available=True)
+        return render(request, 'home/home.html', {'products': product})
+
+
+class ProductDetailView(View):
+    def get(self, request, slug):
+        product = get_object_or_404(Product, slug=slug)
+        return render(request, 'home/product_detail.html', {'products': product})
